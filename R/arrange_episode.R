ae6badb2f6c54ff310cf4c5860451529384ac3c8
Or Moses <orm@inbal.co.il>
2016-10-09 11:32:36 +0300

    added number of words calculation

--- R/arrange_episode.R
+++ R/arrange_episode.R
@@ -3,6 +3,8 @@
 #' A function to arrange a cleaned script (the list that gets back from \code{\link{clean_the_episode}}).
 #' Every row in the dataframe is a different person speaking.
 #' @param ep a list. The list returned from \code{\link{clean_the_episode}} of a single episode.
+#' @importFrom dplyr mutate
+#' @importFrom stringr str_locate_all str_trim
 #' @return a dataframe containing arranged episode script
 arrange_episode <- function(ep) {
   x <- ep$script
@@ -11,9 +13,10 @@ arrange_episode <- function(ep) {
   #get a vector of all speakers
   spks <- vapply(seq_len(nrow(locs)),function(a) substr(x,locs[a,1],locs[a,2]-1),character(1))
   #get a vector of all content
-  content <- vapply(seq_len(nrow(locs)-1),function(a) substr(x,locs[a,2]+1,locs[a+1,1]-1),character(1))
-  content[nrow(locs)] <- substr(x,locs[nrow(locs),2]+1,str_length(x))
+  content <- vapply(seq_len(nrow(locs)-1),function(a) str_trim(substr(x,locs[a,2]+1,locs[a+1,1]-1)),character(1))
+  content[nrow(locs)] <- str_trim(substr(x,locs[nrow(locs),2]+1,str_length(x)))
+  num_words <- vapply(content,function(a) nrow(str_locate_all(a," ")[[1]])+1,numeric(1))
   n <- length(content)
-  data.frame(season = rep(ep$season,n), episode = rep(ep$episode,n), speaker=spks,content=content,
-             total_episode = rep(ep$tot_episode,n))
+  res <- data.frame(season = rep(ep$season,n), episode = rep(ep$episode,n), speaker=spks,content=content,
+             num_words = num_words, total_episode = rep(ep$tot_episode,n),stringsAsFactors = FALSE)
 }
