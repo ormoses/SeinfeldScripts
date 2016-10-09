7f16a41545d5b1cf55d3b1d382b917d62cfcddb9
Or Moses <orm@inbal.co.il>
2016-10-09 15:06:19 +0300

    make a word cloud function

---
+++ R/word_clouds.R
@@ -0,0 +1,39 @@
+#rem_pattern <- content_transformer(function(x, pattern) gsub(pattern, "", x))
+
+#' Make a word cloud from a dataframe
+#'
+#' Make a word cloud from a dataframe
+#' @param df a dataframe to make cloud from
+#' @import tm RColorBrewer wordcloud SnowballC
+#' @importFrom stringr str_replace_all
+#' @importFrom dplyr filter
+#' @return A word cloud chart
+make_cloud_by_name <- function(all_scripts,name,stopwords=FALSE,season="all",episode="all") {
+  df <- all_scripts
+  #filter by name
+  if (name!="all") df <- filter(df,speaker==name)
+  #filter by season
+  if (season!="all") df <- filter(df,season==season)
+  #filter by name
+  if (episode!="all") df <- filter(df,episode==episode)
+
+  df <- df$content
+
+  #make a string
+  df <- paste(df,sep=" ",collapse="")
+  #remove stop words
+  if (stopwords==FALSE) {
+    stopwords_regex <-  paste(stopwords('en'), collapse = '\\b|\\b')
+    stopwords_regex <-  paste0('\\b', stopwords_regex, '\\b')
+    df <-  str_replace_all(df, stopwords_regex, '')
+  }
+  a <- Corpus(VectorSource(df))
+  a <- tm_map(a,stripWhitespace)
+  a <- tm_map(a,removeNumbers)
+  a <- tm_map(a,removePunctuation)
+  a <- tm_map(a,content_transformer(tolower))
+  #a <- tm_map(a,removeWords,stopwords)
+  a <- tm_map(a,stemDocument)
+
+  wordcloud(a, scale=c(4,0.5), max.words=150, random.order=FALSE, rot.per=0.35, use.r.layout=FALSE,colors=brewer.pal(8, "Set1"))
+}
