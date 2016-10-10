a646de98ce1da86b66f677f89f19ffedc0105157
Or Moses <or.moses@gmail.com>
2016-10-10 22:00:08 +0300

    changed dir name

---
+++ inst/TVscripts_app/server.R
@@ -0,0 +1,19 @@
+library(SeinfeldScripts)
+data(seinfeld)
+data(friends)
+
+shinyServer(function(input, output) {
+  output$freq_plot <- renderPlot({
+    if (input$series=="Seinfeld") {
+      x <- seinfeld
+    } else if (input$series=="Friends") {
+      x <- friends
+    }
+    freq <- count_the_speakers(x, type=input$by_what,
+                               season=input$season,
+                               episode=input$episode)
+    plot_the_speakers(freq,15,"Speaker")
+  })
+
+  }
+)
