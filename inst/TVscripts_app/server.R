0e72986abaf53ffc037bfda530b044a0e1f7dde0
Or Moses <orm@inbal.co.il>
2016-10-13 15:37:18 +0300

    formatting

--- inst/TVscripts_app/server.R
+++ inst/TVscripts_app/server.R
@@ -13,8 +13,8 @@ load("data/food_words.rda")
 shinyServer(function(input, output,session) {
   x <- reactive({
     ser <- switch(input$series,
-           "Seinfeld"=seinfeld,
-           "Friends"=friends)
+           "Seinfeld" = seinfeld,
+           "Friends" = friends)
     ser
   })
 
@@ -28,34 +28,37 @@ shinyServer(function(input, output,session) {
     } else {
       season_selected <- "all"
     }
-    updateSelectInput(session,"season",
-                      choices=season_list,
-                      selected=season_selected
+    updateSelectInput(session,
+                      "season",
+                      choices = season_list,
+                      selected = season_selected
     )
     #update the character names according to series
-    spks <- count_the_speakers(scripts,type="num_speaks",season="all",episode="all")
-    spks <- filter(spks,freq>1)
-    updateSelectInput(session,"name",
-                      choices=as.list(c("all",spks$speaker)))
+    spks <- count_the_speakers(scripts,type = "num_speaks",season = "all",episode = "all")
+    spks <- filter(spks,freq > 1)
+    updateSelectInput(session,
+                      "name",
+                      choices = as.list(c("all",spks$speaker)))
   })
 
     #update the episodes on the season
   observeEvent(input$season,{
     scripts <- x()
-    if (input$season=="all") {
+    if (input$season == "all") {
       episode_list <- unique(scripts$episode)
 
     } else {
     episode_list <- scripts %>%
-                    filter(season==as.numeric(input$season)) %>%
+                    filter(season == as.numeric(input$season)) %>%
                     select(episode) %>%
                     unique
     episode_list <- episode_list$episode
 
     }
 
-    updateSelectInput(session,"episode",
-                      choices=as.list(c("all",episode_list)),
+    updateSelectInput(session,
+                      "episode",
+                      choices = as.list(c("all",episode_list)),
                       selected = "all"
     )
   })
@@ -73,17 +76,22 @@ shinyServer(function(input, output,session) {
 
   output$word_plot <- renderPlot({
     scripts <- x()
-    freq <- count_list_of_words(scripts, word_vec=food_words,
-                                name=input$name,
-                                season=input$season,
-                                episode=input$episode)
+    freq <- count_list_of_words(scripts,
+                                word_vec = food_words,
+                                name = input$name,
+                                season = input$season,
+                                episode = input$episode)
     plot_the_speakers(freq,input$bins,"Food")
   })
 
   output$cloud_plot <- renderPlot({
     scripts <- x()
-    make_cloud_by_name(scripts, input$name, season = input$season, episode = input$episode,
-                       stopwords = input$stopwords, stemming = input$stem)
+    make_cloud_by_name(scripts,
+                       input$name,
+                       season = input$season,
+                       episode = input$episode,
+                       stopwords = input$stopwords,
+                       stemming = input$stem)
   })
 
   }
